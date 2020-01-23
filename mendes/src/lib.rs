8cc17368a9834b58b36098dda13265b45a77740c
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-01-23 20:51:07 +0100

    Include more basic utilities

--- mendes/src/lib.rs
+++ mendes/src/lib.rs
@@ -1,4 +1,6 @@
-use http::Response;
+use http::{Response, StatusCode};
+use hyper::header::LOCATION;
+use hyper::Body;
 pub use mendes_derive::dispatch;
 
 pub trait Application {
@@ -6,4 +8,17 @@ pub trait Application {
     type Error;
 
     fn error(&self, error: Self::Error) -> Response<Self::ResponseBody>;
+}
+
+pub fn redirect(status: StatusCode, path: &str) -> Response<Body> {
+    Response::builder()
+        .status(status)
+        .header(LOCATION, path)
+        .body(Body::empty())
+        .unwrap()
+}
+
+pub mod types {
+    pub const HTML: &str = "text/html";
+    pub const JSON: &str = "application/json";
 }
