0bc47403b966b14de3a130de04fee288e9f4c220
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-02-13 20:07:48 +0100

    Simplify tests to use String body

--- mendes/tests/basic.rs
+++ mendes/tests/basic.rs
@@ -2,75 +2,55 @@ use std::sync::Arc;
 
 use async_trait::async_trait;
 use http::{Request, Response, StatusCode};
-use hyper::body::to_bytes;
-use hyper::Body;
 use mendes::{dispatch, handler, Application, ClientError, Context};
 
 #[tokio::test]
 async fn test_nested_rest() {
     let rsp = handle(path_request("/nested/some/more")).await;
     assert_eq!(rsp.status(), StatusCode::OK);
-    assert_eq!(
-        &to_bytes(rsp.into_body()).await.unwrap(),
-        &b"nested rest some/more"[..]
-    );
+    assert_eq!(rsp.into_body(), "nested rest some/more");
 }
 
 #[tokio::test]
 async fn test_nested_right() {
     let rsp = handle(path_request("/nested/right/2018")).await;
     assert_eq!(rsp.status(), StatusCode::OK);
-    assert_eq!(
-        &to_bytes(rsp.into_body()).await.unwrap(),
-        &b"nested right 2018"[..]
-    );
+    assert_eq!(rsp.into_body(), "nested right 2018");
 }
 
 #[tokio::test]
 async fn test_numbered_invalid() {
     let rsp = handle(path_request("/numbered/Foo")).await;
     assert_eq!(rsp.status(), StatusCode::NOT_FOUND);
-    assert_eq!(
-        &to_bytes(rsp.into_body()).await.unwrap(),
-        &b"404 Not Found"[..]
-    );
+    assert_eq!(rsp.into_body(), "404 Not Found");
 }
 
 #[tokio::test]
 async fn test_numbered() {
     let rsp = handle(path_request("/numbered/2016")).await;
     assert_eq!(rsp.status(), StatusCode::OK);
-    assert_eq!(&to_bytes(rsp.into_body()).await.unwrap(), &b"ID = 2016"[..]);
+    assert_eq!(rsp.into_body(), "ID = 2016");
 }
 
 #[tokio::test]
 async fn test_named() {
     let rsp = handle(path_request("/named/Foo")).await;
     assert_eq!(rsp.status(), StatusCode::OK);
-    assert_eq!(
-        &to_bytes(rsp.into_body()).await.unwrap(),
-        &b"Hello, Foo"[..]
-    );
+    assert_eq!(rsp.into_body(), "Hello, Foo");
 }
 
 #[tokio::test]
 async fn test_named_no_arg() {
     let rsp = handle(path_request("/named")).await;
     assert_eq!(rsp.status(), StatusCode::NOT_FOUND);
-    assert_eq!(
-        &to_bytes(rsp.into_body()).await.unwrap(),
-        &b"404 Not Found"[..]
-    );
+    assert_eq!(rsp.into_body(), "404 Not Found");
 }
 
 #[tokio::test]
 async fn test_magic_404() {
     let rsp = handle(path_request("/foo")).await;
     assert_eq!(rsp.status(), StatusCode::NOT_FOUND);
-    assert_eq!(
-        &to_bytes(rsp.into_body()).await.unwrap(),
-        &b"404 Not Found"[..]
-    );
+    assert_eq!(rsp.into_body(), "404 Not Found");
 }
 
 #[tokio::test]
@@ -86,7 +66,7 @@ fn path_request(path: &str) -> Request<()> {
         .unwrap()
 }
 
-async fn handle(req: Request<()>) -> Response<Body> {
+async fn handle(req: Request<()>) -> Response<String> {
     let app = Arc::new(App {});
     let cx = Context::new(app, req);
     App::handle(cx).await
@@ -97,11 +77,11 @@ struct App {}
 #[async_trait]
 impl Application for App {
     type RequestBody = ();
-    type ResponseBody = Body;
+    type ResponseBody = String;
     type Error = Error;
 
     #[dispatch]
-    async fn handle(mut cx: Context<Self>) -> Response<Body> {
+    async fn handle(mut cx: Context<Self>) -> Response<Self::ResponseBody> {
         path! {
             Some("hello") => hello,
             Some("named") => named,
@@ -113,49 +93,49 @@ impl Application for App {
         }
     }
 
-    fn error(&self, err: Error) -> Response<Body> {
+    fn error(&self, err: Error) -> Response<Self::ResponseBody> {
         let Error::Client(err) = err;
         Response::builder()
             .status(StatusCode::from(err))
-            .body(err.to_string().into())
+            .body(err.to_string())
             .unwrap()
     }
 }
 
 #[handler(App)]
-async fn nested_rest(#[rest] path: &str) -> Result<Response<Body>, Error> {
+async fn nested_rest(#[rest] path: &str) -> Result<Response<String>, Error> {
     Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(format!("nested rest {}", path).into())
+        .body(format!("nested rest {}", path))
         .unwrap())
 }
 
 #[handler(App)]
-async fn nested_right(num: usize) -> Result<Response<Body>, Error> {
+async fn nested_right(num: usize) -> Result<Response<String>, Error> {
     Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(format!("nested right {}", num).into())
+        .body(format!("nested right {}", num))
         .unwrap())
 }
 
 #[handler(App)]
-async fn numbered(num: usize) -> Result<Response<Body>, Error> {
+async fn numbered(num: usize) -> Result<Response<String>, Error> {
     Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(format!("ID = {}", num).into())
+        .body(format!("ID = {}", num))
         .unwrap())
 }
 
 #[handler(App)]
-async fn named(name: &str) -> Result<Response<Body>, Error> {
+async fn named(name: &str) -> Result<Response<String>, Error> {
     Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(format!("Hello, {}", name).into())
+        .body(format!("Hello, {}", name))
         .unwrap())
 }
 
 #[handler(App)]
-async fn hello() -> Result<Response<Body>, Error> {
+async fn hello() -> Result<Response<String>, Error> {
     Ok(Response::builder()
         .status(StatusCode::OK)
         .body("Hello, world".into())
