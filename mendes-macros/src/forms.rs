fb787848df9b535298ec26e84b4b070450f7bb8a
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-02-18 20:00:34 +0100

    Extend form building functionality

--- mendes-macros/src/forms.rs
+++ mendes-macros/src/forms.rs
@@ -1,24 +1,55 @@
+use std::mem;
+
 use proc_macro2::Span;
 use quote::{quote, ToTokens};
 use syn::parse::{Parse, ParseStream};
 use syn::punctuated::Punctuated;
 use syn::token::Comma;
 
-pub fn form(meta: &FormMeta, ast: &syn::ItemStruct) -> proc_macro2::TokenStream {
-    let fields = match &ast.fields {
+pub fn form(meta: &FormMeta, ast: &mut syn::ItemStruct) -> proc_macro2::TokenStream {
+    let fields = match &mut ast.fields {
         syn::Fields::Named(fields) => fields,
         _ => panic!("only structs with named fields are supported"),
     };
 
     let mut new = proc_macro2::TokenStream::new();
-    for field in &fields.named {
+    for field in fields.named.iter_mut() {
+        let mut hidden = false;
+        let params = if let Some((i, attr)) = field
+            .attrs
+            .iter_mut()
+            .enumerate()
+            .find(|(_, a)| a.path.is_ident("form"))
+        {
+            let input = mem::replace(&mut attr.tokens, proc_macro2::TokenStream::new());
+            let mut tokens = proc_macro2::TokenStream::new();
+            for (key, value) in syn::parse2::<FieldParams>(input).unwrap().params {
+                if key == "type" && value == "hidden" {
+                    hidden = true;
+                }
+                tokens.extend(quote!(
+                    (#key, #value),
+                ));
+            }
+            field.attrs.remove(i);
+            tokens
+        } else {
+            quote!()
+        };
+
         let name = field.ident.as_ref().unwrap().to_string();
-        let label = syn::LitStr::new(&capitalize(&name), Span::call_site());
+        let label = if hidden {
+            quote!(None)
+        } else {
+            let label = syn::LitStr::new(&capitalize(&name), Span::call_site());
+            quote!(Some(#label.into()))
+        };
+
         let ty = &field.ty;
         new.extend(quote!(
             mendes::forms::Item {
-                label: Some(#label),
-                field: <#ty as mendes::forms::ToField>::to_field(#name.into(), &[]),
+                label: #label,
+                field: <#ty as mendes::forms::ToField>::to_field(#name.into(), &[#params]),
             },
         ));
     }
@@ -41,9 +72,9 @@ pub fn form(meta: &FormMeta, ast: &syn::ItemStruct) -> proc_macro2::TokenStream 
         impl#impl_generics mendes::forms::ToForm for #name#type_generics #where_clause {
             fn to_form() -> mendes::forms::Form {
                 mendes::forms::Form {
-                    action: Some(#action),
+                    action: Some(#action.into()),
                     enctype: None,
-                    method: Some("post"),
+                    method: Some("post".into()),
                     sets: vec![
                         mendes::forms::FieldSet {
                             legend: None,
@@ -52,7 +83,7 @@ pub fn form(meta: &FormMeta, ast: &syn::ItemStruct) -> proc_macro2::TokenStream 
                             ],
                         }
                     ],
-                }
+                }.prepare()
             }
         }
     );
@@ -92,6 +123,32 @@ impl Parse for FormMeta {
             action: action.unwrap(),
             submit: submit.unwrap(),
         })
+    }
+}
+
+pub struct FieldParams {
+    params: Vec<(String, String)>,
+}
+
+impl Parse for FieldParams {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let content;
+        let _ = syn::parenthesized!(content in input);
+        let metas = Punctuated::<syn::NestedMeta, Comma>::parse_terminated(&content)?;
+
+        let mut params = vec![];
+        for meta in metas {
+            if let syn::NestedMeta::Meta(syn::Meta::NameValue(pair)) = meta {
+                let key = pair.path.get_ident().unwrap().to_string();
+                let value = pair.lit.into_token_stream().to_string();
+                let value = value.trim_matches('"').to_string();
+                params.push((key, value))
+            } else {
+                unreachable!()
+            }
+        }
+
+        Ok(Self { params })
     }
 }
 
