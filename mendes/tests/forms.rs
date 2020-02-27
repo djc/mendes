62eb6d03010aa130c5555028e28843d273729d49
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-02-27 09:39:59 +0100

    Add some basic testing of form macros

--- mendes/tests/forms.rs
+++ mendes/tests/forms.rs
@@ -6,18 +6,23 @@ use mendes::forms::{form, ToField, ToForm};
 use serde::{Deserialize, Serialize};
 
 #[test]
-fn test_form() {
+fn test_generate() {
     let form = SomeForm::to_form();
+    let form = form.set("name", "hi").unwrap();
+    let _ = form.to_string();
 }
 
+#[allow(dead_code)]
 #[form(action = "/assets/new", submit = "Create")]
-#[derive(serde::Deserialize)]
+#[derive(Deserialize)]
 struct SomeForm<'a> {
     name: Cow<'a, str>,
     amount: u32,
     rate: f32,
     byte: u8,
+    #[form(item = "Group")]
     test: bool,
+    #[form(item = "Group")]
     options: Options,
     #[cfg(feature = "chrono")]
     outstanding_principal_date: chrono::NaiveDate,
