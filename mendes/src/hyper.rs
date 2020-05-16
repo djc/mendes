82e3812719ee9a86e153bfa644adb80d82c8e38b
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-05-16 20:17:37 +0200

    Move hyper module into a separate file

---
+++ mendes/src/hyper.rs
@@ -0,0 +1,34 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use futures_util::future::FutureExt;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::Body;
+
+use super::{Application, Context};
+use crate::application::Server;
+
+#[async_trait]
+impl<A> Server for A
+where
+    A: Application<RequestBody = Body, ResponseBody = Body> + Send + Sync + 'static,
+{
+    type ServerError = hyper::Error;
+
+    async fn serve(self: Self, addr: &SocketAddr) -> Result<(), hyper::Error> {
+        let app = Arc::new(self);
+        hyper::Server::bind(addr)
+            .serve(make_service_fn(move |_| {
+                let app = app.clone();
+                async {
+                    Ok::<_, Infallible>(service_fn(move |req| {
+                        let cx = Context::new(app.clone(), req);
+                        A::handle(cx).map(Ok::<_, Infallible>)
+                    }))
+                }
+            }))
+            .await
+    }
+}
--- mendes/src/lib.rs
+++ mendes/src/lib.rs
@@ -15,6 +15,9 @@ pub mod cookies;
 #[cfg(feature = "forms")]
 mod form;
 
+#[cfg(feature = "hyper")]
+mod hyper;
+
 #[cfg(feature = "uploads")]
 mod multipart;
 
@@ -23,44 +26,6 @@ pub mod forms {
     pub use super::form::*;
     #[cfg(feature = "uploads")]
     pub use super::multipart::{from_form_data, File};
-}
-
-#[cfg(feature = "hyper")]
-pub mod hyper {
-    use std::convert::Infallible;
-    use std::net::SocketAddr;
-    use std::sync::Arc;
-
-    use async_trait::async_trait;
-    use futures_util::future::FutureExt;
-    use hyper::service::{make_service_fn, service_fn};
-    use hyper::Body;
-
-    use super::{Application, Context};
-    use crate::application::Server;
-
-    #[async_trait]
-    impl<A> Server for A
-    where
-        A: Application<RequestBody = Body, ResponseBody = Body> + Send + Sync + 'static,
-    {
-        type ServerError = hyper::Error;
-
-        async fn serve(self: Self, addr: &SocketAddr) -> Result<(), hyper::Error> {
-            let app = Arc::new(self);
-            hyper::Server::bind(addr)
-                .serve(make_service_fn(move |_| {
-                    let app = app.clone();
-                    async {
-                        Ok::<_, Infallible>(service_fn(move |req| {
-                            let cx = Context::new(app.clone(), req);
-                            A::handle(cx).map(Ok::<_, Infallible>)
-                        }))
-                    }
-                }))
-                .await
-        }
-    }
 }
 
 pub mod types {
