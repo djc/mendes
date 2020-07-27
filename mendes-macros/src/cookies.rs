8d73df711a682eb626352975d226caeccd5dcc03
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-07-27 14:21:55 +0200

    Implement new Application-based cookie API

--- mendes-macros/src/cookies.rs
+++ mendes-macros/src/cookies.rs
@@ -7,24 +7,13 @@ use quote::quote;
 pub fn cookie(ast: &syn::ItemStruct) -> proc_macro2::TokenStream {
     let ident = &ast.ident;
     let name = syn::LitStr::new(&ident.to_string(), Span::call_site());
-    let cookie = quote!(
+    quote!(
         impl mendes::cookies::CookieData for #ident {
             const NAME: &'static str = #name;
 
             fn expires() -> Option<std::time::Duration> {
                 Some(std::time::Duration::new(60 * 60 * 6, 0))
             }
-            fn from_header(key: &mendes::cookies::Key, req: &mendes::http::HeaderMap) -> Option<Self> {
-                mendes::cookies::extract(key, req)
-            }
-            fn to_string(self, key: &mendes::cookies::Key) -> Result<mendes::http::HeaderValue, ()> {
-                mendes::cookies::store(key, self)
-            }
-            fn tombstone() -> Result<mendes::http::HeaderValue, ()> {
-                mendes::cookies::tombstone(Self::NAME)
-            }
         }
-    );
-
-    cookie
+    )
 }
