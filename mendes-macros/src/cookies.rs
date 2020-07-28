3770854f363f65de28d13b402dfaa47f6738d937
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-07-28 21:13:00 +0200

    Store cookie name in associated const

--- mendes-macros/src/cookies.rs
+++ mendes-macros/src/cookies.rs
@@ -9,17 +9,19 @@ pub fn cookie(ast: &syn::ItemStruct) -> proc_macro2::TokenStream {
     let name = syn::LitStr::new(&ident.to_string(), Span::call_site());
     let cookie = quote!(
         impl mendes::cookies::CookieData for #ident {
+            const NAME: &'static str = #name;
+
             fn expires() -> Option<std::time::Duration> {
                 Some(std::time::Duration::new(60 * 60 * 6, 0))
             }
             fn from_header(key: &mendes::cookies::Key, req: &mendes::http::HeaderMap) -> Option<Self> {
-                mendes::cookies::extract(#name, key, req)
+                mendes::cookies::extract(key, req)
             }
             fn to_string(self, key: &mendes::cookies::Key) -> Result<mendes::http::HeaderValue, ()> {
-                mendes::cookies::store(#name, key, self)
+                mendes::cookies::store(key, self)
             }
             fn tombstone() -> Result<mendes::http::HeaderValue, ()> {
-                mendes::cookies::tombstone(#name)
+                mendes::cookies::tombstone(Self::NAME)
             }
         }
     );
