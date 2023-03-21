e41c0d3e90fa51d0af8905892eb24084e2cc21b0
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-03-21 09:55:01 +0100

    Simplify FieldParams parser code

--- mendes-macros/src/util.rs
+++ mendes-macros/src/util.rs
@@ -9,25 +9,23 @@ pub struct FieldParams {
 
 impl Parse for FieldParams {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let metas = Punctuated::<syn::Meta, Comma>::parse_terminated(input)?;
-
-        let mut params = vec![];
-        for meta in metas {
-            match meta {
-                syn::Meta::NameValue(meta) => {
-                    let key = meta.path.get_ident().unwrap().to_string();
-                    let value = meta.value.into_token_stream().to_string();
-                    let value = value.trim_matches('"').to_string();
-                    params.push((key, value))
-                }
-                syn::Meta::Path(path) => {
-                    let key = path.get_ident().unwrap().to_string();
-                    params.push((key, "true".into()));
-                }
-                _ => unimplemented!(),
-            }
-        }
-
-        Ok(Self { params })
+        Ok(Self {
+            params: Punctuated::<syn::Meta, Comma>::parse_terminated(input)?
+                .into_iter()
+                .map(|meta| match meta {
+                    syn::Meta::NameValue(meta) => {
+                        let key = meta.path.get_ident().unwrap().to_string();
+                        let value = meta.value.into_token_stream().to_string();
+                        let value = value.trim_matches('"').to_string();
+                        (key, value)
+                    }
+                    syn::Meta::Path(path) => {
+                        let key = path.get_ident().unwrap().to_string();
+                        (key, "true".into())
+                    }
+                    _ => unimplemented!(),
+                })
+                .collect(),
+        })
     }
 }
